0f1c65063548aae0dbcbb06cb00c850cc5a4c1f8
cone011 <32248132+cone011@users.noreply.github.com>
2023-01-16 07:50:41 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
-    );
+function App() {
+  const [repeat, SetRepeat] = useState("");
+  function ValueChange(e) {
+    SetRepeat(e.target.value);
   }
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input
+        type="text"
+        placeholder="Empieza a escribir algo"
+        onChange={ValueChange}
+      />
+      <p className="repeater">{repeat}</p>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Un repetidor</h1>
+//         <input type="text" placeholder="Empieza a escribir algo" />
+//         <p className="repeater"></p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
